e240822f252e7edb1d0b29c26b71b5288cee21a6
Bartek <bartosz.adamczuk@gmail.com>
2019-11-18 23:06:14 +0100

    Modele

--- listownik/models.py
+++ listownik/models.py
@@ -3,14 +3,9 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    nick = models.CharField(max_length=30)
     email = models.EmailField()
     password = models.CharField(max_length=30)
-
-class List(models.Model):
-    name = models.CharField(max_length=30)
-
 
 class Shop(models.Model):
     name = models.CharField(max_length=30)
@@ -19,7 +14,38 @@ class Measure(models.Model):
     name = models.CharField(max_length=30)
     short = models.CharField(max_length=10)
 
+class Type(models.Model):
+    name = models.CharField(max_length=30)
+
 class Product(models.Model):
     name = models.CharField(max_length=30)
-    unit_of_measure = models.ForeignKey(Measure,on_delete=models.CASCADE())
-    shop = models.ForeignKey(Shop,on_delete=models.CASCADE())
+    unit_of_measure = models.ForeignKey(Measure,on_delete=models.CASCADE)
+    idType = models.ForeignKey(Type,on_delete=models.CASCADE)
+    idShop = models.ForeignKey(Shop,on_delete=models.CASCADE)
+
+class List(models.Model):
+    name = models.CharField(max_length=30)
+    favorite = models.BooleanField(default=False)
+    idOwner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class List_Product(models.Model):
+    idList = models.ForeignKey(List,on_delete=models.CASCADE)
+    idProduct = models.ForeignKey(Product, on_delete=models.CASCADE)
+    isBought = models.BooleanField(default=False)
+
+class Shop_Product(models.Model):
+    idShop = models.ForeignKey(Shop,on_delete=models.CASCADE)
+    idProduct = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+class Subscription(models.Model):
+    idOwner = models.ForeignKey(User,on_delete=models.CASCADE, related_name='Owner')
+    idList = models.ForeignKey(List,on_delete=models.CASCADE)
+    idSubscription = models.ForeignKey(User,on_delete=models.CASCADE,related_name='Subscriber')
+
+class User_Subscription(models.Model):
+    idUser = models.ForeignKey(User,on_delete=models.CASCADE)
+    idSubscription = models.ForeignKey(Subscription,on_delete=models.CASCADE)
+
+class List_Subscriber(models.Model):
+    idList = models.ForeignKey(List, on_delete=models.CASCADE)
+    idSubscription = models.ForeignKey(Subscription,on_delete=models.CASCADE)
