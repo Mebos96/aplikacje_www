875a0f40b19c5da61cb44ecbd864cb1e4fc07338
Bartosz Adamczuk <bartosz.adamczuk@gmail.com>
2019-12-21 20:34:27 +0100

    Fetchowanie poprawka

--- Inzynierka/settings.py
+++ Inzynierka/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +147,4 @@ CORS_ORIGIN_ALLOW_ALL = True
 #     "http://127.0.0.1:9000",
 #     "http://localhost:19002"
 # ]
+django_heroku.settings(locals())
